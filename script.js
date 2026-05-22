4880f28b7f257496b7a15e9527ed5831026d7206
ArenasITSE <EduardoArenas@escarcega.tecnm.mx>
2026-05-22 11:27:11 -0600

    congreso gastronomia

---
+++ script.js
@@ -0,0 +1,36 @@
+const targetDate = new Date("October 27, 2026 00:00:00").getTime();
+
+const timer = setInterval(function(){
+
+    const now = new Date().getTime();
+
+    const distance = targetDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+
+    const minutes = Math.floor(
+        (distance % (1000 * 60 * 60)) / (1000 * 60)
+    );
+
+    const seconds = Math.floor(
+        (distance % (1000 * 60)) / 1000
+    );
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if(distance < 0){
+
+        clearInterval(timer);
+
+        document.querySelector(".countdown").innerHTML =
+            "<h2>¡El Congreso ha comenzado!</h2>";
+    }
+
+},1000);
